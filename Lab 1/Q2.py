15374fa7f0c98fae41185a33217b0cf3d78baf1a
S. Sara Ali Zaidi <142868034+saraali13@users.noreply.github.com>
2024-08-21 09:58:48 +0500

    Create Q2.py

---
+++ Lab 1/Q2.py
@@ -0,0 +1,12 @@
+num1=float(input("enter first number"))
+num2=float(input("enter second number"))
+op=input("enter the operation you want to perform")
+if op=='+':
+    print("Sum is: ",abs(-num1-num2))
+elif op=='-':
+    print("Diffrence is: ",num1-num2)
+elif op== '*':
+    print("Product is: ",num1*num2)
+elif op=='/':
+    print("Quotient is:",num1/num2)
+
