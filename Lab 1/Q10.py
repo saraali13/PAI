a61cda1cf1cecc2965410c624a7f3436dc72ca35
S. Sara Ali Zaidi <142868034+saraali13@users.noreply.github.com>
2024-08-24 20:17:50 +0500

    Update Q10.py

--- Lab 1/Q10.py
+++ Lab 1/Q10.py
@@ -1,4 +1,6 @@
-num = list(map(int, input("Enter numbers: ").split())) #map will covert each string to an int number then then list
+num = list(map(int, input("Enter numbers: ").split())) 
+#map will covert each string to an int number then then list
+#split will split a single string input to list of strings"1 2 3" to "1" "2" "3"
 
 largest_num = max(num)
 print("The largest number is:", largest_num)
