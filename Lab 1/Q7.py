d4857fe446570a521abddaeedc4e6504c4928062
S. Sara Ali Zaidi <142868034+saraali13@users.noreply.github.com>
2024-08-24 19:51:11 +0500

    Create Q7.py

---
+++ Lab 1/Q7.py
@@ -0,0 +1,7 @@
+word = input("Enter a word ")
+reversedword = ""
+
+for char in word:
+    reversedword = char + reversedword
+#if we,ll enter "was" so frist ch to be stroed will be w then a then s so the output will be saw
+print("Reversed word ", reversedword)
