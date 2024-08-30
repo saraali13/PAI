ae24df0f8d89335f4cc1af51c5210c96f02abae7
S. Sara Ali Zaidi <142868034+saraali13@users.noreply.github.com>
2024-08-30 18:59:00 +0500

    Update Q4.py

--- Lab 2/Task/Q4.py
+++ Lab 2/Task/Q4.py
@@ -1,1 +1,6 @@
+def employee(name,monthly_salary=10000):
+    monthly_salary=monthly_salary-monthly_salary*0.02
+    print(f"Name of Employee: {name}, Salary after tax deduction : {monthly_salary}")
 
+employee("Sara",23000)
+employee("Ali")
