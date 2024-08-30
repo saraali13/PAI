657313e224633a9c5f662bb4f3b5c0f0ad59ab72
S. Sara Ali Zaidi <142868034+saraali13@users.noreply.github.com>
2024-08-30 15:55:01 +0500

    Create Q5.py

---
+++ Lab 2/practice/Q5.py
@@ -0,0 +1,9 @@
+name = input("Enter your name? ")
+print(f"Hello {name} ")
+ans = input("I hope you are fine, let me know can I help you?")
+if(ans == "yes" or ans == "Yes"):
+    probl  = input("Share your problem with us: ")
+    print('"Thanks for your feedback,we will resolve your problem"')
+
+else:
+    print(center("Okay! Good to see you , stay connected"))
