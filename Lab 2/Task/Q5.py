9a50c283c36d1394432ade8d5ad520267351e7a7
S. Sara Ali Zaidi <142868034+saraali13@users.noreply.github.com>
2024-08-30 19:13:48 +0500

    Update Q5.py

--- Lab 2/Task/Q5.py
+++ Lab 2/Task/Q5.py
@@ -1,1 +1,13 @@
+def factorial(num):
+    if num == 0:
+        return 1
+    elif num >= 1:
+        fact = 1
+        for i in range(1, num+1):
+            fact *= i
+        return fact
+
+n = int(input("Enter a number: "))
+print(factorial(n))
+
 
