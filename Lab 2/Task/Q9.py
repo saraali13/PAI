7e6f1a27c5858e212dc46d0ccf207f2984676b0f
S. Sara Ali Zaidi <142868034+saraali13@users.noreply.github.com>
2024-08-31 21:24:13 +0500

    Update Q9.py

--- Lab 2/Task/Q9.py
+++ Lab 2/Task/Q9.py
@@ -1,12 +1,1 @@
-def count_words_and_characters(filename):
-    with open(filename, 'r') as file:
-        text = file.read()
-
-        num_characters = len(text)
-        num_words = len(text.split())
-
-        print(f"Number of characters: {num_characters}")
-        print(f"Number of words: {num_words}")
-
-count_words_and_characters("samplefile.txt")
 #text file should have the same directory as the python project one
