62cdfbc3aa621c34a24e625d29bfe77fc19f482c
S. Sara Ali Zaidi <142868034+saraali13@users.noreply.github.com>
2024-09-04 09:40:33 +0500

    Create Q4.py

---
+++ Lab 3/Q4.py
@@ -0,0 +1,21 @@
+name = input('Enter your name: ')
+cnic = int(input('Enter your cnic number: '))
+age = int(input('Enter your age: '))
+salary = int(input('Enter your salary: '))
+
+l1 = [name,cnic,age,salary]
+try:
+    with open('example','w') as file:
+        file.write("Bio Data of the Employee:\n")
+        for i in range(len(l1)):
+            file.write(str(f"{l1[i]} ,"))
+except Exception as e:
+    print(str(e))
+
+number = int(input('Enter your contact number: '))
+
+try:
+    with open('example','a') as file:
+        file.write(str(number))
+except Exception as e:
+    print(str(e))
