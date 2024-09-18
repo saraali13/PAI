fb0b74e5071cfb8001673081cd339a74aa552907
S. Sara Ali Zaidi <142868034+saraali13@users.noreply.github.com>
2024-09-18 08:20:20 +0500

    Update Q1.py

--- Lab 5/Q1.py
+++ Lab 5/Q1.py
@@ -1,1 +1,22 @@
+class Vehicle:
+    def __init__(self, seating):
+        self.seating_capacity = seating
 
+    def fare(self):
+        total_fare = self.seating_capacity * 100
+        return total_fare
+
+
+class Bus(Vehicle):
+    def __init__(self, seating):
+        super().__init__(seating)
+        self.seating_capacity = seating
+
+    def fare_Bus(self):
+        fare = self.fare() + (self.fare() * 10 / 100)
+        print(f"Total Bus fare: {fare}")
+
+
+B1 = Bus(34)
+print(B1.fare())
+B1.fare_Bus()
