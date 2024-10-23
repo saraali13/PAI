751ab3cd3ee7f3ca482fb3d332e6fff7ce496649
S. Sara Ali Zaidi <142868034+saraali13@users.noreply.github.com>
2024-10-23 20:58:38 +0500

    Update Task 3.py

--- Lab 9/Task 3.py
+++ Lab 9/Task 3.py
@@ -1,10 +1,10 @@
 from matplotlib import pyplot as plt
 import pandas as pd
 
-df = pd.read_csv("new.csv")
+df = pd.read_csv("Iris.csv")
 x = df["Species"]
 y = df["PetalWidthCm"]
-color = ['green', 'purple', 'yellow', 'blue','red']
-plt.scatter(x, y, c=color , marker="*")
+colors = {'Iris-setosa': 'red', 'Iris-versicolor': 'blue', 'Iris-virginica': 'green'}
+plt.scatter(df['Species'], df['PetalWidthCm'], c=df['Species'].map(colors))
 
 plt.show()
