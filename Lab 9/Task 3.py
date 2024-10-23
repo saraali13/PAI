d51cd22dd26470f8fac3026b8b8a14be30d3cd28
S. Sara Ali Zaidi <142868034+saraali13@users.noreply.github.com>
2024-10-23 20:59:02 +0500

    Update Task 3.py

--- Lab 9/Task 3.py
+++ Lab 9/Task 3.py
@@ -5,6 +5,6 @@ df = pd.read_csv("Iris.csv")
 x = df["Species"]
 y = df["PetalWidthCm"]
 colors = {'Iris-setosa': 'red', 'Iris-versicolor': 'blue', 'Iris-virginica': 'green'}
-plt.scatter(df['Species'], df['PetalWidthCm'], c=df['Species'].map(colors))
+plt.scatter(df['Species'], df['PetalWidthCm'], c=df['Species'].map(colors), marker="*")
 
 plt.show()
