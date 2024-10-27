85f96ab95bfeb41e236800679aba419e2e44b3aa
S. Sara Ali Zaidi <142868034+saraali13@users.noreply.github.com>
2024-10-27 17:15:44 +0500

    Update Q1.py

--- Lab 9/Q1.py
+++ Lab 9/Q1.py
@@ -1,1 +1,19 @@
+import matplotlib.pyplot as plt
 
+students = ["Sara", "Ali", "Sakina", "Abbas", "Haider", "Zainab"]
+heights = [70, 80, 73, 84, 87, 76]
+clr = ["Purple", "red", "blue", "green", "yellow", "pink"]
+
+# Bar Chart
+plt.figure()  # for creating 2 separate figs
+plt.bar(students, heights, color=clr)
+plt.xlabel('Students')
+plt.ylabel('Height')
+plt.title('Student Height data')
+plt.show()
+
+# Pie Chart
+plt.figure()  # first we'll close the bar graph then pie graph will appear automatically
+plt.pie(heights, labels=students, colors=clr)
+plt.title('Student Heights Distribution')
+plt.show()
