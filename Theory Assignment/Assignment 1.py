9d7cc5f7229369f66649fe36efc79f6acdca677f
S. Sara Ali Zaidi <142868034+saraali13@users.noreply.github.com>
2024-10-28 09:04:36 +0500

    Update Assignment 1.py

--- Theory Assignment/Assignment 1.py
+++ Theory Assignment/Assignment 1.py
@@ -1,35 +1,17 @@
 class Animal:
     def __init__(self, name, age, habitat, available):
-        self._name = name
-        self._age = age
-        self._habitat = habitat
-        self._is_available = available
-
-    def get_name(self):
-        return self._name
-
-    def set_name(self, value):
-        self._name = value
-
-    def get_age(self):
-        return self._age
-
-    def set_age(self, value):
-        self._age = value
-
-    def get_habitat(self):
-        return self._habitat
-
-    def set_habitat(self, value):
-        self._habitat = value
+        self.name = name
+        self.age = age
+        self.habitat = habitat
+        self.is_available = available
 
     def change_availability_status(self, available):
-        self._is_available = available
+        self.is_available = available
 
     def display_info(self):
         print("Displaying Animal's Info:")
-        print(f"Name: {self._name}\nAge: {self._age}\nHabitat: {self._habitat}")
-        if self._is_available:
+        print(f"Name: {self.name}\nAge: {self.age}\nHabitat: {self.habitat}")
+        if self.is_available:
             print("Status: Available for viewing")
         else:
             print("Status: In quarantine")
@@ -38,70 +20,34 @@ class Animal:
 class Mammal(Animal):
     def __init__(self, name, age, habitat, available, fur, diet):
         super().__init__(name, age, habitat, available)
-        self._fur_length = fur
-        self._diet_type = diet
-
-    def get_fur_length(self):
-        return self._fur_length
-
-    def set_fur_length(self, value):
-        self._fur_length = value
-
-    def get_diet_type(self):
-        return self._diet_type
-
-    def set_diet_type(self, value):
-        self._diet_type = value
+        self.fur_length = fur
+        self.diet_type = diet
 
     def display_info(self):
         super().display_info()
-        print(f"Fur Length: {self._fur_length}\nDiet Type: {self._diet_type}\n")
+        print(f"Fur Length: {self.fur_length}\nDiet Type: {self.diet_type}\n")
 
 
 class Bird(Animal):
     def __init__(self, name, age, habitat, available, wingspan, flight):
         super().__init__(name, age, habitat, available)
-        self._wingspan = wingspan
-        self._flight_altitude = flight
-
-    def get_wingspan(self):
-        return self._wingspan
-
-    def set_wingspan(self, value):
-        self._wingspan = value
-
-    def get_flight_altitude(self):
-        return self._flight_altitude
-
-    def set_flight_altitude(self, value):
-        self._flight_altitude = value
+        self.wingspan = wingspan
+        self.flight_altitude = flight
 
     def display_info(self):
         super().display_info()
-        print(f"Wingspan: {self._wingspan}\nFlight Altitude: {self._flight_altitude}\n")
+        print(f"Wingspan: {self.wingspan}\nFlight Altitude: {self.flight_altitude}\n")
 
 
 class Reptile(Animal):
     def __init__(self, name, age, habitat, available, scale, venomous):
         super().__init__(name, age, habitat, available)
-        self._scale_pattern = scale
-        self._venomous_status = venomous
-
-    def get_scale_pattern(self):
-        return self._scale_pattern
-
-    def set_scale_pattern(self, value):
-        self._scale_pattern = value
-
-    def get_venomous_status(self):
-        return self._venomous_status
-
-    def set_venomous_status(self, value):
-        self._venomous_status = value
-
+        self.scale_pattern = scale
+        self.venomous_status = venomous
+        
     def display_info(self):
         super().display_info()
-        print(f"Scale Pattern: {self._scale_pattern}\nVenomous Status: {self._venomous_status}\n")
+        print(f"Scale Pattern: {self.scale_pattern}\nVenomous Status: {self.venomous_status}\n")
 
 
 # Sample Zoo
