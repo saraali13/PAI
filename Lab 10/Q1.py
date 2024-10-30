16b33d4010f85ff8250fce954cc0863369fe75e0
S. Sara Ali Zaidi <142868034+saraali13@users.noreply.github.com>
2024-10-30 09:29:23 +0500

    Update Q1.py

--- Lab 10/Q1.py
+++ Lab 10/Q1.py
@@ -1,1 +1,31 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+
+df = pd.read_csv("heart.csv")
+
+plt.figure()
+sns.histplot(data=df,x="fbs")
+plt.show()
+
+plt.figure()
+sns.boxplot(x="fbs",y="age",data=df)
+plt.show()
+
+plt.figure()
+sns.scatterplot(data=df,x="fbs",y="age")
+plt.show()
+
+plt.figure()
+sns.lineplot(y=df["age"],x=df["fbs"])
+plt.show()
+
+plt.figure()
+sns.displot(df)
+plt.ylabel("fbs")
+plt.show()
+
+plt.figure()
+sns.pairplot(df,hue="age")
+plt.show()
 
