b49ba1f925bfac1b670fa2097ec4d4baf8b6fbca
S. Sara Ali Zaidi <142868034+saraali13@users.noreply.github.com>
2024-12-03 15:06:36 +0500

    Update Q1.r

--- Lab 13/Q1.r
+++ Lab 13/Q1.r
@@ -1,11 +1,19 @@
-calculate_grade <- function(score) {
-  if (score >= 90) {
+calculate_grade <- function(score) 
+{
+  if (score >= 90) 
+  {
     return("A")
-  } else if (score >= 80) {
+  } 
+  else if (score >= 80) 
+  {
     return("B")
-  } else if (score >= 70) {
+  }
+  else if (score >= 70) 
+  {
     return("C")
-  } else {
+  } 
+  else 
+  {
     return("Fail")
   }
 }
